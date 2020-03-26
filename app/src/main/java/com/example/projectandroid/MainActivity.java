0806dce915268835361a1f284646a1ffa498df45
Le hong phong <idtmnman@gmail.com>
2020-03-26 22:39:19 +0700

    em phong xem navigation bate

--- app/src/main/java/com/example/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
