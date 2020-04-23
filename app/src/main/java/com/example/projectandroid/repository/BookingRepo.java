f9855e72ddaf6f4753d4eb88d427f8893d378503
Hoang <hoanglxph09371@fpt.edu.vn>
2020-04-24 00:36:15 +0700

    update db

--- app/src/main/java/com/example/projectandroid/repository/BookingRepo.java
+++ app/src/main/java/com/example/projectandroid/repository/BookingRepo.java
@@ -6,23 +6,27 @@ import android.os.AsyncTask;
 import androidx.room.Room;
 
 import com.example.projectandroid.dao.BookingDAO;
+import com.example.projectandroid.dao.BookingStatusDAO;
 import com.example.projectandroid.dao.RoomDAO;
 import com.example.projectandroid.database.AppDatabase;
 import com.example.projectandroid.model.Booking;
+import com.example.projectandroid.model.BookingStatus;
 import com.example.projectandroid.model.Rooms;
 
 import java.util.List;
 
 public class BookingRepo {
     private BookingDAO bookingDAO;
+    private BookingStatusDAO bookingStatusDAO;
 
     public BookingRepo(Context context) {
         AppDatabase database = Room.databaseBuilder(context, AppDatabase.class, "hotlindbling").allowMainThreadQueries().build();
         bookingDAO = database.bookingDAO();
+        bookingStatusDAO = database.bookingStatusDAO();
     }
 
     public void insert(Booking booking) {
-        new InsertBookingAsyncTask(bookingDAO).execute(booking);
+        new InsertBookingAsyncTask(bookingDAO, bookingStatusDAO).execute(booking);
     }
 
     public void update(Booking booking) {
@@ -37,7 +41,6 @@ public class BookingRepo {
         return bookingDAO.getAllBooking();
     }
 
-
     public Booking getBookingById(int idBooking) {
         return bookingDAO.getBookingById(idBooking).get(0);
     }
@@ -46,16 +49,18 @@ public class BookingRepo {
     private static class InsertBookingAsyncTask extends AsyncTask<Booking, Void, Void> {
 
         BookingDAO bookingDAO;
+        BookingStatusDAO bookingStatusDAO;
 
-        InsertBookingAsyncTask(BookingDAO bookingDAO) {
+        InsertBookingAsyncTask(BookingDAO bookingDAO, BookingStatusDAO bookingStatusDAO) {
             this.bookingDAO = bookingDAO;
+            this.bookingStatusDAO = bookingStatusDAO;
         }
 
 
         @Override
         protected Void doInBackground(Booking... bookings) {
             if (bookingDAO.insertBooking(bookings)[0] > 0) {
-                String idRoom = bookings[0].getIdRoom();
+                bookingStatusDAO.insertStatusDAO(new BookingStatus(bookings[0].getId(), "Online"));
             }
 
             return null;
